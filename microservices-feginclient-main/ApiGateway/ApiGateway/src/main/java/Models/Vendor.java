91828cb35cb24515553857f23f6db76527d71cef
venumadhav7777 <124698370+venumadhav7777@users.noreply.github.com>
2025-04-10 09:34:18 +0530

    Add files via upload

---
+++ microservices-feginclient-main/ApiGateway/ApiGateway/src/main/java/Models/Vendor.java
@@ -0,0 +1,15 @@
+package Models;
+
+
+@Entity
+public class Vendor {
+
+
+
+    private Long vendorId;
+    private String name;
+    private String serviceType;
+    private String contactInfo;
+    private String location;
+    private double rating;
+}
